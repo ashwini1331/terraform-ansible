28166c0d8613b2ad93b2c8fea07d67c67f5a109c
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-04-23 23:54:19 +0530

    Update ansiblelocal.sh

--- ansiblelocal.sh
+++ ansiblelocal.sh
@@ -1,6 +1,5 @@
-add-apt-repository ppa:ansible/ansible
-curl -sSL https://gitlab.com/roybhaskar9/devops/raw/master/coding/ansible/installansiblerepo.sh | sh
-apt-get update
-apt-get install -y ansible
+sudo add-apt-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
 wget https://raw.githubusercontent.com/ashwini1331/terraform-ansible/master/playbook.yml
-ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
+sudo ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
